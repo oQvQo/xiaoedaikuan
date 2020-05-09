f662e3ff65779c110ff22ffa3a176d9c8bfc2708
oQvQo <1262120596@qq.com>
2020-05-09 14:55:05 +0800

    贷后预警新增查看修改

--- src/router/EarlyWarning/index.js
+++ src/router/EarlyWarning/index.js
@@ -1,9 +1,5 @@
 // 贷后预警
 export default [
-	{
-		path:'/EarlyWarningShow',
-		component:()=>import('../../views/EarlyWarning/show')
-    },
     {
 		path:'/EarlyWarningAdd',
 		component:()=>import('../../views/EarlyWarning/add')
--- src/views/EarlyWarning/add/index.vue
+++ src/views/EarlyWarning/add/index.vue
@@ -39,7 +39,7 @@
         </el-select>
       </el-form-item>
 
-      <el-form-item label="预警阶段" prop="region">
+      <el-form-item label="预警阶段" prop="region5">
         <el-select v-model="ruleForm.region5" placeholder="请选择">
           <el-option label="三天" value="OneDays"></el-option>
           <el-option label="七天" value="SevenDays"></el-option>
@@ -47,51 +47,45 @@
       </el-form-item>
 
       <el-form-item label="活动时间" required>
-        <el-col :span="11">
-          <el-form-item prop="date1">
-            <el-date-picker
-              type="date"
-              placeholder="选择日期"
-              v-model="ruleForm.date1"
-              style="width: 100%;"
-            ></el-date-picker>
-          </el-form-item>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-form-item prop="date2">
-            <el-time-picker placeholder="选择时间" v-model="ruleForm.date2" style="width: 100%;"></el-time-picker>
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-
-      <el-form-item label="即时配送" prop="delivery">
-        <el-switch v-model="ruleForm.delivery"></el-switch>
-      </el-form-item>
-
-      <el-form-item label="活动性质" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-          <el-checkbox label="地推活动" name="type"></el-checkbox>
-          <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-          <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-        </el-checkbox-group>
+        <el-input-number v-model="num" @change="handleChange" :min="1" :max="10" label="描述文字"></el-input-number>
       </el-form-item>
 
       <el-form-item label="特殊资源" prop="resource">
         <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="线上品牌商赞助"></el-radio>
-          <el-radio label="线下场地免费"></el-radio>
+          <el-radio label="一次"></el-radio>
+          <el-radio label="每天一次"></el-radio>
         </el-radio-group>
       </el-form-item>
 
-      <el-form-item label="活动形式" prop="desc">
-        <el-input type="textarea" v-model="ruleForm.desc"></el-input>
+      <el-form-item label="特殊资源" prop="resource">
+        <el-radio-group v-model="ruleForm.resource2">
+          <el-radio label="10:00"></el-radio>
+          <el-radio label="14:00"></el-radio>
+          <el-radio label="18:00"></el-radio>
+          <el-radio label="22:00"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="接收成员" prop="region6">
+        <el-select v-model="ruleForm.region6" placeholder="请选择" @change="item=>sz.push(item)">
+          <el-option label="小熊" value="小熊"></el-option>
+          <el-option label="小张" value="小张"></el-option>
+          <el-option label="小蒋" value="小蒋"></el-option>
+          <el-option label="小李" value="小李"></el-option>
+        </el-select>
+        <br>
+        <span v-for="(item,index) in sz" :key="index" @click="sz.splice(sz.findIndex(e=>e===item),1)">{{item}} </span>
+      </el-form-item>
+
+      <el-form-item label="是否启用" prop="delivery">
+        <el-switch v-model="ruleForm.delivery" active-color="#333334" inactive-color="#d3d3d3"></el-switch>
+        <br />
+        <span>{{ruleForm.delivery?'打开':'关闭'}}</span>
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-        <el-button @click="resetForm('ruleForm')">重置</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+        <el-button @click="resetForm('ruleForm')">返回</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -100,18 +94,30 @@
 <script>
 export default {
   data() {
+     
     return {
+      sz:[],
+      num: 1,
       ruleForm: {
         name: "",
         region: "",
+        region2: "",
+        region3: "",
+        region4: "",
+        region5: "",
+
         date1: "",
         date2: "",
         delivery: false,
         type: [],
         resource: "",
-        desc: ""
+        resource2: "",
+        desc: "",
+        num: 1
       },
+
       rules: {
+        delivery: { required: true },
         name: [
           { required: true, message: "请输入活动名称", trigger: "blur" },
           { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
@@ -121,6 +127,7 @@ export default {
         region3: [{ message: "请选择", trigger: "change" }],
         region4: [{ message: "请选择", trigger: "change" }],
         region5: [{ required: true, message: "请选择", trigger: "change" }],
+        region6: [{ required: true, message: "请选择", trigger: "change" }],
         date1: [
           {
             type: "date",
@@ -145,10 +152,8 @@ export default {
             trigger: "change"
           }
         ],
-        resource: [
-          { required: true, message: "请选择活动资源", trigger: "change" }
-        ],
-        desc: [{ required: true, message: "请填写活动形式", trigger: "blur" }]
+        resource: [{ required: true, message: "请选择", trigger: "change" }],
+        resource2: [{ required: true, message: "请选择", trigger: "change" }]
       }
     };
   },
@@ -165,6 +170,9 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
+    },
+    handleChange(value) {
+      console.log(value);
     }
   }
 };
